7f21f7eb460f215b31fc0c52656cb75d78e3ff5f
Matthias Sebastian Sort <mss@dynamicweb.dk>
2024-04-05 15:05:28 +0200

    added missing Close-function in the DestinationWriter plus logic for DiscardDuplicates.

--- src/OrderProvider.cs
+++ src/OrderProvider.cs
@@ -328,7 +328,7 @@ public class OrderProvider : BaseSqlProvider, IParameterOptions
                     Logger.Log("Starting import to temporary table for " + mapping.DestinationTable.Name + ".");
                     using (var reader = job.Source.GetReader(mapping))
                     {
-                        var writer = new OrderDestinationWriter(mapping, Connection, Logger, SkipFailingRows);
+                        var writer = new OrderDestinationWriter(mapping, Connection, Logger, SkipFailingRows, DiscardDuplicates);
                         var columnMappings = mapping.GetColumnMappings();
                         while (!reader.IsDone())
                         {
